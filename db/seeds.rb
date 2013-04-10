c348b843b7c37b4240eaa6ec60275e250dd7d106
John Crossley <hello@phpcodemonkey.com>
2013-04-10 23:19:22 +0100

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# User.create([{username: 'admin', password: 'password', email: 'hello@phpcodemonkey.com'}])
+User.create([{username: 'admin', password: 'password', email: 'hello@phpcodemonkey.com'}])
