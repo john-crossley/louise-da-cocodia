c48c75d21ea8a9e9ce43306a43146bd8fd0653c1
John Crossley <hello@phpcodemonkey.com>
2013-04-11 13:29:51 +0100

    Changed the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-User.create([{username: 'admin', password: 'password', email: 'hello@phpcodemonkey.com'}])
