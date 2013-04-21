2c7b88c667b3d4ea3b4b1d568feeb055c81bd8c1
John Crossley <hello@phpcodemonkey.com>
2013-04-21 11:10:48 +0100

    notes for Richard

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ LouiseDaCocodia::Application.configure do
   config.consider_all_requests_local = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
